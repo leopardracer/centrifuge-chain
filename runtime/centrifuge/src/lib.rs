7545682dac84bdc1a62de6e61c1605b4c962953e
leopardracer <136604165+leopardracer@users.noreply.github.com>
2025-06-27 16:51:07 +0300

    Update lib.rs

--- runtime/centrifuge/src/lib.rs
+++ runtime/centrifuge/src/lib.rs
@@ -297,7 +297,7 @@ parameter_types! {
 
 impl cumulus_pallet_parachain_system::Config for Runtime {
 	type CheckAssociatedRelayNumber = cumulus_pallet_parachain_system::RelayNumberStrictlyIncreases;
-	// Using weights for recomended hardware
+	// Using weights for recommended hardware
 	type DmpQueue = frame_support::traits::EnqueueWithOrigin<MessageQueue, RelayOrigin>;
 	type OnSystemEvent = ();
 	type OutboundXcmpMessageSource = XcmpQueue;
@@ -318,7 +318,7 @@ parameter_types! {
 impl pallet_message_queue::Config for Runtime {
 	type HeapSize = sp_core::ConstU32<{ 64 * 1024 }>;
 	type MaxStale = sp_core::ConstU32<8>;
-	// Using weights for recomended hardware
+	// Using weights for recommended hardware
 	#[cfg(feature = "runtime-benchmarks")]
 	type MessageProcessor =
 		pallet_message_queue::mock_helpers::NoopMessageProcessor<AggregateMessageOrigin>;
